dafabd26e19fba7914e264f0fc1403e5b8b5f70e
Shehryar <shehryarkashan@gmail.com>
2024-05-11 16:33:56 +0500

    change

--- src/data/index.js
+++ src/data/index.js
@@ -4,19 +4,19 @@ export const services = [
   {
     id: 1,
     title: "Outpatient",
-    des: "over 3000 mental health visits in 2023",
+    des: "over 3000 mental health visits in 2023.",
     pic: images.outPatient,
   },
   {
     id: 2,
     title: "Inpatient",
-    des: "residential mental health facility opens in 2024",
+    des: "residential mental health facility opens in 2024.",
     pic: images.inPatient,
   },
   {
     id: 3,
     title: "Intesnive outpatient",
-    des: "9 hours weekly treatment for adolescents and adults",
+    des: "9 hours weekly treatment for adolescents and adults.",
     pic: images.inTensiveOutPatient,
   },
 ];
